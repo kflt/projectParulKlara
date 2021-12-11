1aba59c695024e5c5f4910feedb462b6ce73c921
kflt <79709156+kflt@users.noreply.github.com>
2021-12-11 19:45:14 +0100

    Update mipslabmain.c

--- mipslabmain.c
+++ mipslabmain.c
@@ -43,19 +43,20 @@ void show_menu(void){
 		quicksleep(1000000);
 
 		do{
-			  display_string(0, "               ");
-			  display_string(1, "      PLAY     ");
-			  display_string(2, "  SW4 WALLS ON ");
-			  display_string(3, "               ");
-			  display_update();
+		  	display_string(0, "               ");
+		  	display_string(1, "      PLAY     ");
+		  	display_string(2, "  SW4 WALLS ON ");
+		  	display_string(3, "               ");
+		  	display_update();
 
-			  if ((getbtns() & 1) || (getbtns() & 2) || (getbtns() & 4) || (getbtns() & 8)){
-						game_reset(getsw());
-						selectedFunc = &play_game;
-						b = false;
+		 	if ((getbtns() & 1) || (getbtns() & 2) || (getbtns() & 4) || (getbtns() & 8)){
+			     
+			     game_reset(getsw());
+			     selectedFunc = &play_game;
+			     b = false;
 			}
 
-		   } while(b);
+		 } while(b);
 }
 
 
@@ -63,34 +64,34 @@ void show_menu(void){
    Loops until the game is over, and then sets the function pointer to menu */
 void play_game(void){
 
-	   if (timeout == stored_timeout)
-	    	 return;
+	   if(timeout == stored_timeout)
+	       return;
 
 	   stored_timeout = timeout;
 
 	   pressed_button = getbtns();
 
-           if (pressed_button)
-	       stored_button = pressed_button;
+           if(pressed_button)
+	      stored_button = pressed_button;
 
-	   if (old_button != stored_button){
-	       old_button = stored_button;
+	   if(old_button != stored_button){
+	      old_button = stored_button;
 
-	       if (stored_button & 1)
-		   direction = DIRECTION_RIGHT;
+	       if(stored_button & 1)
+		  direction = DIRECTION_RIGHT;
 	       
-	       else if (stored_button & 2)
-		   direction = DIRECTION_DOWN;
+	       else if(stored_button & 2)
+		       direction = DIRECTION_DOWN;
 		
-	       else if (stored_button & 8)
-	           direction = DIRECTION_LEFT;
+	       else if(stored_button & 8)
+	               direction = DIRECTION_LEFT;
 	
 	       else
-	           direction = DIRECTION_UP;
+	            direction = DIRECTION_UP;
 	   }
 
 	   /* Return 0 = Game over
 	      Return 1 = Continue playing */
-	   if (!game_iteration(direction))
-	        selectedFunc = &show_menu;
+	   if(!game_iteration(direction))
+	       selectedFunc = &show_menu;
 }
